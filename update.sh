2511a050318f2508720c95bb5cead7603e190584
C.C. <45786458+MajoSissi@users.noreply.github.com>
2025-02-14 11:39:24 +0800

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# 定义输入和输出文件
+input_file="trackers_link.txt"
+output_file="trackers.txt"
+
+# 清空输出文件
+> "$output_file"
+
+# 逐行读取输入文件中的链接
+while IFS= read -r url; do
+    # 使用 curl 获取链接内容，并追加到输出文件中
+    curl -s "$url" >> "$output_file"
+done < "$input_file"
+
+# 去掉输出文件中的空行
+sed -i '/^$/d' "$output_file"
